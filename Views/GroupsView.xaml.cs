66d8eb5745261c604c8f4a9ed5516633dfeeede5
drnopoh2810-spec <dr.nopoh2810@gmail.com>
2026-06-21 06:31:28 +0300

    Add Views/GroupsView.xaml.cs

---
+++ Views/GroupsView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace EnglishLearningApp.Views
+{
+    public partial class GroupsView : UserControl
+    {
+        public GroupsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
