738e966ef4652f6f77e75fa9c9a1c793f46ddc35
drnopoh2810-spec <dr.nopoh2810@gmail.com>
2026-06-21 06:31:34 +0300

    Add Views/SentencesView.xaml.cs

---
+++ Views/SentencesView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace EnglishLearningApp.Views
+{
+    public partial class SentencesView : UserControl
+    {
+        public SentencesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
