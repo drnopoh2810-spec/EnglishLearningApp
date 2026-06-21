d56845497dd1630ddcaa7ba3acf20f3d45868a27
drnopoh2810-spec <dr.nopoh2810@gmail.com>
2026-06-21 06:31:29 +0300

    Add Views/ImportExportView.xaml.cs

---
+++ Views/ImportExportView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace EnglishLearningApp.Views
+{
+    public partial class ImportExportView : UserControl
+    {
+        public ImportExportView()
+        {
+            InitializeComponent();
+        }
+    }
+}
